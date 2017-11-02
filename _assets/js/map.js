387530db996e3954162050488b0cafaec73ae846
ol-bashkin <ol.bashkin@gmail.com>
2017-11-03 00:48:06 +0300

    добавил страничку студии в бурге

---
+++ _assets/js/map.js
@@ -0,0 +1,37 @@
+function initMap() {
+  'use strict';
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 46.5202688, lng: 6.6343104},
+      zoom: 21,
+      disableDefaultUI: true,
+      gestureHandling: 'greedy',
+      styles: [
+        {
+          featureType: 'poi.business',
+          stylers: [{visibility: 'off'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'labels.icon',
+          stylers: [{visibility: 'off'}]
+        }
+      ]
+    }),
+    
+    markerSize = new google.maps.Size(150, 90),
+    marker = new google.maps.Marker({
+
+      position: {lat: 46.5202688, lng: 6.6343104},
+
+      icon: {
+        url: '../../_assets/img/_studios/bourg/icons/studio_marker.png',
+        size: markerSize,
+        scaledSize: markerSize,
+        anchor: new google.maps.Point(75, 87)
+      },
+      optimized: false,
+      map: map
+
+    });
+  
+}
