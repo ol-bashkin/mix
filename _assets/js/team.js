4bf5695c489b19c9ac784965024cd77fa452d34e
ol-bashkin <ol.bashkin@gmail.com>
2017-11-09 02:17:29 +0300

    доделал карусель в команде и разместил фотки

--- _assets/js/team.js
+++ _assets/js/team.js
@@ -3,38 +3,65 @@ function init() {
   var slider = document.getElementsByClassName('js-team')[0],
     container = document.getElementsByClassName('js-container')[0],
     slides = document.getElementsByClassName('js-trainer'),
+    slidesCount = slides.length,
+    sliderWidth = slidesCount * 331 + slidesCount * 12 + 'px',
     leftArrow = document.getElementsByClassName('js-switch-left')[0],
     rightArrow = document.getElementsByClassName('js-switch-right')[0],
     currentSlide = 0,
     sliderStyle = window.getComputedStyle(slider),
+    containerStyle = window.getComputedStyle(container),
+    viewPort = containerStyle.width,
     timer = '',
     timerTime = '';
   
+  window.addEventListener('resize', function () {
+    viewPort = containerStyle.width;
+    console.log(viewPort);
+  }, true);
+  console.log(slides[0]);
+  slider.style.width = sliderWidth;
+  console.log(viewPort + '!');
   function slideRight() {
-    var curPos = parseInt(slider.style.left, 10);
-    if (currentSlide < slides) {
-      slider.style.left = curPos - 343 + 'px';
-      currentSlide += 1;
-    } else {
-      slider.style.left = 0;
-      currentSlide = 0;
-    }
-    console.log(currentSlide);
+    var movedImg,
+      count = 0,
+      slides = document.getElementsByClassName('js-trainer'),
+      timer = setInterval(function () {
+        if (count === 20) {
+          slider.style.left = parseInt(slider.style.left, 10) - 3 + 'px';
+          clearInterval(timer);
+        } else {
+          slider.style.left = parseInt(slider.style.left, 10) - 340 / 20 + 'px';
+          console.log(slider.style.left);
+          count += 1;
+        }
+      }, 20);
+    
+    window.setTimeout(function () {
+      movedImg = slider.removeChild(slides[0]);
+      slider.appendChild(movedImg);
+      slider.style.left = parseInt(slider.style.left, 10) + 343 + 'px';
+    }, 500);
   }
   
   function slideLeft() {
-    var curPos = parseInt(slider.style.left, 10);
-    
-    if (currentSlide > 0) {
-      
-      slider.style.left = curPos + 343 + 'px';
-      currentSlide -= 1;
-    } else {
-      slider.style.left = -2058 + 'px';
-      currentSlide = slides;
-    }
-    
-    console.log(currentSlide);
+    var movedImg,
+      count = 0,
+      slides = document.getElementsByClassName('js-trainer'),
+      timer;
+    movedImg = slider.removeChild(slides[slidesCount - 1]);
+    slider.insertBefore(movedImg, slides[0]);
+    slider.style.left = parseInt(slider.style.left, 10) - 343 + 'px';
+    timer = setInterval(function () {
+      if (count === 20) {
+        slider.style.left = parseInt(slider.style.left, 10) + 3 + 'px';
+        clearInterval(timer);
+      } else {
+        slider.style.left = parseInt(slider.style.left, 10) + 340 / 20 + 'px';
+        console.log(slider.style.left);
+        count += 1;
+      }
+    }, 20);
+
   }
   
   slides = slides.length - 1;
