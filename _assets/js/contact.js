41a9c30f661023400a7dc00cb805479a152c0c1c
ol-bashkin <ol.bashkin@gmail.com>
2017-11-22 14:10:47 +0300

    ajax

--- _assets/js/contact.js
+++ _assets/js/contact.js
@@ -1,3 +1,24 @@
+function postStuff(inputs, popup) {
+  "use strict";
+  // Create our XMLHttpRequest object
+  var hr = new XMLHttpRequest(),
+  // Create some variables we need to send to our PHP file
+    url = "../../../_assets/php/ajax.php",
+    vars = 'name' + inputs[0].value;
+  hr.open("POST", url, true);
+  hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // Access the onreadystatechange event for the XMLHttpRequest object
+  hr.onreadystatechange = function () {
+    if (hr.readyState === 4 && hr.status === 200) {
+      if (!popup.classList.contains('popup--is-visible')) {
+        popup.classList.add('popup--is-visible');
+      }
+    }
+  };
+  // Send the data to PHP now... and wait for response to update the status div
+  hr.send(vars); // Actually execute the request
+}
+
 function init() {
   "use strict";
   var inputs = document.getElementsByClassName('js-input'),
@@ -29,9 +50,7 @@ function init() {
       return previus && current;
     });
     if (valid) {
-      if (!popup.classList.contains('popup--is-visible')) {
-        popup.classList.add('popup--is-visible');
-      }
+      postStuff(inputs, popup);
     }
   });
  
